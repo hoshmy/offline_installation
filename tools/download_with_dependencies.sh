5e8afd2f60a271676cbd11acc2520cb3c2af70a8
dev <dee>
2021-01-26 07:28:00 +0200

    Add download with dependencies

---
+++ tools/download_with_dependencies.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+PACKAGES=$1
+apt-get download $(apt-cache depends --recurse --no-recommends --no-suggests \
+  --no-conflicts --no-breaks --no-replaces --no-enhances \
+  --no-pre-depends ${PACKAGES} | grep "^\w")
